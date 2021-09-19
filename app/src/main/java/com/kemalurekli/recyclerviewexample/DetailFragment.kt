a812d8cad24bf609b5013dfca54c09ef4e479f94
kemalurekli <kemalurekli@gmail.com>
2021-09-19 21:20:08 +0300

    added navigation view binding and layouts

---
+++ app/src/main/java/com/kemalurekli/recyclerviewexample/DetailFragment.kt
@@ -0,0 +1,40 @@
+package com.kemalurekli.recyclerviewexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kemalurekli.recyclerviewexample.databinding.FragmentDetailBinding
+
+
+class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
